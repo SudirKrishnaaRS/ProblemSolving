fb8ed2185f9588327c257a695a08b3097c05f9b8
Sudir Krishnaa RS <67383465+SudirKrishnaaRS@users.noreply.github.com>
2021-06-12 19:24:19 +0530

    Create solution.java

---
+++ Maximum Target Hits in Sequence/solution.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Your Code Here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		int brr[]=new int[n];
+		
+		for(int i=0;i<n;i++)
+		    arr[i]=sc.nextInt();
+		   
+		int c=0;
+		for(int i=0;i<n-1;i++)
+		{
+		    if(arr[i+1]>arr[i])
+		    {
+		        c++;
+		        brr[i]=c;
+		    }
+		    else
+		    { 
+		        c=0;
+		        brr[i]=c;
+		    }
+		}
+		
+		Arrays.sort(brr);
+		System.out.println(brr[n-1]);
+	}
+}
